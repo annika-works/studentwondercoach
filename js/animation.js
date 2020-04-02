7f317716963f0af7f0fe1bba0a1c0c14566901c8
Annika Hallerberg <annika.hallerberg@gmx.de>
2020-04-02 16:33:46 +0200

    animation set

--- js/animation.js
+++ js/animation.js
@@ -1,3 +1,35 @@
+const runScripts = () => {
+    const headers = document.querySelectorAll('h1, .intro, .vfz')
+
+  
+
+    const observer = new IntersectionObserver(
+      entries => {
+        entries.forEach(entry => {
+          if (entry.intersectionRatio >= 0.1) {
+            entry.target.classList.add('appear')
+          } else {
+            entry.target.classList.remove('appear')
+          }
+        })
+      },
+      {
+        threshold: [0, 0.1, 1]
+      }
+    )
+  
+    headers.forEach(header => {
+      observer.observe(header)
+    })
+  }
+  
+  runScripts()
+
+
+
+
+
+
 // document.addEventListener("mousemove", event => {
 
 //     const x = event.pageX
