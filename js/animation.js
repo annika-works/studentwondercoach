e487f0eedf1668c202982a45a7819e8b5b1bc034
Annika Hallerberg <annika.hallerberg@gmx.de>
2020-04-03 18:01:06 +0200

    contact form styled up, media queries set

--- js/animation.js
+++ js/animation.js
@@ -25,25 +25,3 @@ const runScripts = () => {
   
   runScripts()
 
-
-
-
-
-
-// document.addEventListener("mousemove", event => {
-
-//     const x = event.pageX
-//     const y = event.pageY
-
-//     const target = document.querySelector(".test")
-//     const targetCoords = target.getBoundingClientRect()
-
-//     const targetX = targetCoords.left + (target.offsetWidth / 2)
-//     const targetY = targetCoords.top + (target.offsetHeight / 2)
-
-//     const angleX = (targetY - y) / 90
-//     const angleY = (targetX - x) / -90
-
-//     target.style.transform = "rotateX("+ angleX + "deg) rotateY("+ angleY + "deg)"
-
-// });
