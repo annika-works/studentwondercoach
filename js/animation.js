de3b4ea2c5611e0d97cc1a3064099458f341ccdf
Annika Hallerberg <annika.hallerberg@gmx.de>
2020-04-11 19:43:45 +0200

    updated mobile animation

--- js/animation.js
+++ js/animation.js
@@ -1,16 +1,15 @@
 const runScripts = () => {
     const headers = document.querySelectorAll('h1, .intro, .vfz')
 
-  
-
     const observer = new IntersectionObserver(
       entries => {
         entries.forEach(entry => {
           if (entry.intersectionRatio >= 0.1) {
             entry.target.classList.add('appear')
-          } else {
-            entry.target.classList.remove('appear')
           }
+            // else {
+          //   entry.target.classList.remove('appear')
+          // }
         })
       },
       {
@@ -24,4 +23,53 @@ const runScripts = () => {
   }
   
   runScripts()
+
+
+// toggle mobile
+
+var bodyTag = document.querySelector('body')
+var hamburger = document.querySelector(".hamburger");
+var mobilemenu = document.querySelector(".mobile-wrapper");
+var mobilecontent = document.querySelector(".mobile-container");
+var navlinks = document.querySelectorAll('nav.mobile a');
+var logoTag = document.querySelector('div.logo-container');
+
+hamburger.addEventListener("click", function() {
+  mobilemenu.classList.toggle("slide-in");
+
+  if(mobilemenu.classList.contains('slide-in')) {
+    hamburger.classList.add("is-active");
+    mobilecontent.classList.add("appear");
+    bodyTag.style.overflow = 'hidden';
+    // window.scrollTo(0, 0);
+
+    if(window.pageYOffset > 0) {
+
+      var delay = setTimeout(() => {
+        logoTag.classList.add('logo-container-mobile')
+        document.querySelector('div.logo-container img').classList.add("appear");
+      }, 500)
+      // logoTag.classList.add('logo-container-mobile')
+    };
+
+  } else {
+    hamburger.classList.remove('is-active');
+    mobilecontent.classList.remove("appear");
+    bodyTag.style.overflow = 'auto';
+    logoTag.classList.remove('logo-container-mobile')
+    document.querySelector('div.logo-container img').classList.remove("appear");
+  }
+
+});
+
+navlinks.forEach( link => {
+  link.addEventListener("click", () => {
+    mobilemenu.classList.remove("slide-in")
+    hamburger.classList.remove('is-active')
+    mobilecontent.classList.remove("appear");
+    bodyTag.style.overflow = 'auto';
+    logoTag.classList.remove('logo-container-mobile')
+    document.querySelector('div.logo-container img').classList.remove("appear");
+  })
+});
 
